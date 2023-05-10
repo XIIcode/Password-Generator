c1b9d815121d15af8adf0a2f3a8dc885151b8780
XIIcode <111756707+XIIcode@users.noreply.github.com>
2023-05-10 23:11:00 +0300

    Create main.c

---
+++ V_1.0.0/main.c
@@ -0,0 +1,15 @@
+#include "generator.h"
+#include <stdio.h>
+void intro()
+{
+    printf("\t\t\tPASSWORD GENERATOR V1.0.0\n");
+    printf("\t\t\tCREATED ON 5/10/2023\n");
+    printf("\t\t\tCREATED BY XIICODE\n");
+    printf("\t\t\tCOPY,USE AND ENJOY.\n\n\n");
+}
+int main()
+{
+    intro();
+    get_password();
+}
+
